c5f851f21b508ed4b34f2532db5a10f055bb8470
georgikolev17 <g.t.kolev1@gmail.com>
2019-11-23 17:58:00 +0200

    First commit

---
+++ Online-Chess/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Online_Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] chessBoard = new char[8, 8]
+            {
+                {'R' , 'K', 'B', 'K', 'Q', 'B', 'K', 'R'},
+                {'P' , 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                {' ' , ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ' , ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ' , ' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                {' ' , ' ', ' ', ' ', ' ', ' ', ' ', ' '}, 
+                {'p' , 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                {'r' , 'k', 'b', 'k', 'q', 'b', 'k', 'r'}
+            };
+
+            while (true)
+            {
+                string startPosition = Console.ReadLine();
+                string finalPosition = Console.ReadLine();
+
+                int startRow = int.Parse(startPosition[1].ToString()) - 1;
+                int startCol = 7 - ((int)startPosition[0] - 97);
+                int finalRow = int.Parse(finalPosition[1].ToString()) - 1;
+                int finalCol = 7 - ((int)finalPosition[0] - 97);
+
+                chessBoard[finalRow, finalCol] = chessBoard[startRow, startCol];
+                chessBoard[startRow, startCol] = ' ';
+
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        Console.Write(chessBoard[i, j]+" ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
